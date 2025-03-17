4fc2992a0d10019aa113413aeec1164bde4f25f8
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-17 18:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const freq={};
+
+	for(let char of str){
+		freq[char]=(freq[char]||0)+1;
+	}
+
+	for(let char of str){
+		if(freq[char]===1) return char;
+	}
+
+	return null
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+console.log(firstNonRepeatedChar('aabbcdd'))
+console.log(firstNonRepeatedChar('aabbcc'))
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
